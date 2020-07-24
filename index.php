46d1930188e3d6e91ca60b48ccebfadd1b7156a6
Alex Palomes <alejandropalomes@hotmail.com>
2020-07-24 17:25:50 +0200

    html structure index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <!-- TODO Application entry point. Login view -->
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
