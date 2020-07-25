b38b60ab5584230e5323cfc5e9028a6e278759c7
beron <bleink@outlook.es>
2020-07-25 22:23:25 +0200

    added event to button save in src/dashboard.php and function selectChanges to select row of inputs and function save Employee to send the inputs of a row to src/library/employeeController.php

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,16 +1,44 @@
 document.getElementById('add-employee').addEventListener('click',addEmployee)
+let newRows = []
+let updateRows = []
+const rowsSection = $('#employees-rows') 
 function addEmployee(){
+    newRows.push("a")
     const columns = document.getElementById('employees-columns').firstChild.children
-    const rows = $('#employees-rows') 
     let inputs = "<tr>"
     for(let index = 0; columns.length-1 > index; index++){
-        inputs += '<td><input class="form-control" type="text" data-column="' + columns[index].dataset.column  + 
-        '" placeholder="' + columns[index].textContent + '" /></td>'
+        let column = columns[index].dataset.column
+        inputs += '<td><input class="form-control" type="text" data-column="' + column  + 
+        '" placeholder="' + column + '" /></td>'
     }
     inputs += '<td><button class="btn btn-block text-warning" onclick="cancelNew()"><i class="fas fa-window-close"></i></button></td>'
     inputs += '</tr>'
-    rows.prepend(inputs)
+    rowsSection.prepend(inputs)
 }
 function cancelNew(){
+    newRows.pop()
     event.currentTarget.parentElement.parentElement.remove()
+}
+document.getElementById("save").addEventListener("click",selectChanges)
+function selectChanges(){
+    const rows = rowsSection[0].children
+    for(let index in newRows){
+        saveEmployee(rowsSection[0].children[index].children, true)
+    }
+}
+function saveEmployee(employeeCells, newEmployee = false){
+    let employeeData = {}
+    employeeData['new'] = newEmployee
+    for(let index = 0; employeeCells.length > index+1; index++){
+        let employeeInput = employeeCells[index].children[0]
+        employeeData[employeeInput.placeholder] = employeeInput.value
+    }
+    $.ajax({
+        method:'POST',
+        url:"library/employeeController.php",
+        data:employeeData,
+        success:function(response){
+            console.log(employeeData)
+        }
+    })
 }
