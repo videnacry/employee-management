5035831e482d2523a631e9e259b739afd000fc1e
Alex Palomes <alejandropalomes@hotmail.com>
2020-07-30 02:42:57 +0200

    refactor login files

--- src/library/loginController.php
+++ src/library/loginController.php
@@ -1,40 +1,7 @@
 <?php
 
-$admins = json_decode(file_get_contents('../../resources/users.json'));
+include('loginManager.php');
 
-foreach ($admins->users as $key => $user) {
-    if($user->email === $_POST['email'] && password_verify($_POST['pwd'], $user->password)){
-        session_start();
-
-        $_SESSION['logged'] = true;
-        $_SESSION['userId'] = $user->userId;
-        $_SESSION['username'] = $user->name;
-        $_SESSION['email'] = $user->email;
-        $_SESSION['logTime'] = time();
-
-        if(isset($_SESSION['wrong-email'])) unset($_SESSION['wrong-email']);
-        if(isset($_SESSION['wrong-pwd'])) unset($_SESSION['wrong-pwd']);
-
-        $url = '../dashboard.php';
-        header('Location: ' . $url);
-        die();
-    }elseif($user->email === $_POST['email'] && !password_verify($_POST['pwd'], $user->password)){
-        session_start();
-
-        $_SESSION['wrong-pwd'] = true;
-        if(isset($_SESSION['wrong-email'])) unset($_SESSION['wrong-email']);
-
-        $url = '../../index.php';
-        header('Location: ' . $url);
-        die();
-    }elseif ($user->email !== $_POST['email'] && $key+1 === count($admins->users)) {
-        session_start();
-
-        $_SESSION['wrong-email'] = true;
-        if(isset($_SESSION['wrong-pwd'])) unset($_SESSION['wrong-pwd']);
-
-        $url = '../../index.php';
-        header('Location: ' . $url);
-        die();
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    userLogin($_POST['email'], $_POST['pwd']);
 }
